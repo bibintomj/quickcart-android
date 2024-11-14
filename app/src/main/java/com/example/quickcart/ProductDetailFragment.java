a07e7669975864ddf7e06ba9487afa46a66c0daf
Bibin Joseph <bibintomj@gmail.com>
2024-11-14 15:34:17 -0500

    Added Splash Images, Banner, Cart UI setup

--- app/src/main/java/com/example/quickcart/ProductDetailFragment.java
+++ app/src/main/java/com/example/quickcart/ProductDetailFragment.java
@@ -15,7 +15,9 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.appcompat.widget.Toolbar;
@@ -30,6 +32,22 @@ public class ProductDetailFragment extends Fragment {
     TextView titleTextView;
     ImageButton cartButton;
 
+    ImageView bannerImageView;
+    TextView priceTextView;
+    TextView productNameTextView;
+    TextView soldByTextView;
+
+    View starBar;
+
+    TextView returnTextView;
+    TextView productDescription;
+
+    Button decreaseCountButton;
+    TextView countTextView;
+    Button increaseCountButton;
+
+    Button addButton;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -41,10 +59,55 @@ public class ProductDetailFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_product_detail, container, false);
         String productName = getArguments().getString("product", "No Product");
+        extractComponentReferencesInView(view);
+        setupListenersOnView(view);
+        updateStarBar(4);
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+
+    // Example: Set star colors based on a rating
+    private void updateStarBar(float rating) {
+        ImageView[] stars = {
+                starBar.findViewById(R.id.star1),
+                starBar.findViewById(R.id.star2),
+                starBar.findViewById(R.id.star3),
+                starBar.findViewById(R.id.star4),
+                starBar.findViewById(R.id.star5)
+        };
+
+        for (int i = 0; i < stars.length; i++) {
+            if (rating > i) {
+                stars[i].setColorFilter(ContextCompat.getColor(requireContext(), R.color.yellow));
+            } else {
+                stars[i].setColorFilter(ContextCompat.getColor(requireContext(), R.color.gray));
+            }
+        }
+    }
+
+    public void extractComponentReferencesInView(View view) {
         backButton = view.findViewById(R.id.backButton);
         titleTextView = view.findViewById(R.id.titleTextView);
         cartButton = view.findViewById(R.id.cartButton);
 
+        bannerImageView = view.findViewById(R.id.bannerImageView);
+        priceTextView = view.findViewById(R.id.priceTextView);
+        productNameTextView = view.findViewById(R.id.productNameTextView);
+        soldByTextView = view.findViewById(R.id.soldByTextView);
+        starBar = view.findViewById(R.id.starBar);
+        returnTextView = view.findViewById(R.id.returnTextView);
+        productDescription = view.findViewById(R.id.productDescription);
+        decreaseCountButton = view.findViewById(R.id.decreaseCountButton);
+        countTextView = view.findViewById(R.id.countTextView);
+        increaseCountButton = view.findViewById(R.id.increaseCountButton);
+        addButton = view.findViewById(R.id.addButton);
+    }
+
+    public void setupListenersOnView(View view) {
         backButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View button) {
@@ -57,12 +120,5 @@ public class ProductDetailFragment extends Fragment {
                 Navigation.findNavController(view).navigate(R.id.navigateToCart);
             }
         });
-        return view;
-    }
-
-    @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-
     }
 }
